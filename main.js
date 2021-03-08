97cd08941091d97520c0db181fd6f91b185eb746
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-08 23:57:59 +0700

    update javascript

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let show = true
+
+const showForm = ()=>{
+
+    //tangkap element note form
+    const nf = document.getElementById('note_form')
+
+    //check nilai variable show
+    if(show === true){
+        nf.style.transform = 'translateX(0)'
+        show = false
+    }else{
+        nf.style.transform = 'translateX(-250px)'
+        show = true
+    }
+
+}
