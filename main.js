4d67e332c9f5ea9d4d333907b902cc3bc4affa0d
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-10 23:48:21 +0700

    update fadli

--- main.js
+++ main.js
@@ -16,3 +16,64 @@ const showForm = ()=>{
     }
 }
 
+window.onload = ()=>{
+    const cc = document.querySelector('.card_container')
+    fetch('http://localhost:3000/notes_data', {
+        method : 'GET',
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        data.map((e)=>{
+            cc.innerHTML += `
+                <div class="card">
+                    <span class="material-icons delete" id="delete_icon" onclick="deleteNote(${e.id})">delete</span>
+                    <span class="material-icons edit" id="edit_icon">edit</span>
+                    <h3 class="card_title">${e.title}</h3>
+                    <p class="card_body">${e.body}</p>
+                </div>
+            `
+        })
+    })
+}
+
+window.deleteNote = (id)=>{
+    fetch(`http://localhost:3000/notes_data/${id}`, {
+        method : 'DELETE',
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        }
+    }).then(res => res.json())
+    .then(data => {
+        window.location.reload()
+    })
+}
+
+window.submitNote = (event)=>{
+    event.preventDefault()
+    let tit = event.target.title.value
+    let bod = event.target.noteBody.value
+
+    fetch(`http://localhost:3000/notes_data`, {
+        method : 'POST',
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body : JSON.stringify({
+            title : tit,
+            body : bod
+        })
+    }).then(res => res.json())
+    .then(data => {
+        window.location.reload()
+    })
+    
+    
+}
+
